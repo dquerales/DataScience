180ded22ca500c07ea70b14fa5a82127861b3b11
Daniel Querales <d.querales@gmail.com>
2022-04-13 22:19:25 -0400

    more reorder fixes

--- Python/Heroku/request.py
+++
@@ -1,9 +0,0 @@
-#required lib
-import requests
-
-#request to api
-url = 'http://localhost:8080/api'
-r = requests.post(url, json={'X': ['I love Star Wars', 'I hate Harry Potter', 'I like it']})
-
-#result
-print(r.json())
